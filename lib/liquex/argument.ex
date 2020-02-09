20eaced77398d6979ba87ab39b4090bf5f309081
Mark Glenn <markglenn@gmail.com>
2020-02-08 23:17:59 -0600

    Add argument parser

---
+++ lib/liquex/argument.ex
@@ -0,0 +1,22 @@
+defmodule Liquex.Argument do
+  def eval([literal: literal], _context), do: literal
+
+  def eval([field: accesses], context) do
+    do_eval(context, accesses)
+  end
+
+  defp do_eval(value, []), do: value
+  defp do_eval(nil, _), do: nil
+
+  defp do_eval(value, [{:key, key} | tail]) do
+    value
+    |> Map.get(key)
+    |> do_eval(tail)
+  end
+
+  defp do_eval(value, [{:accessor, accessor} | tail]) do
+    value
+    |> Enum.at(accessor)
+    |> do_eval(tail)
+  end
+end
