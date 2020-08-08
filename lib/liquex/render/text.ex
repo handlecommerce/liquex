6ae9d06d5145b8e8d40f0961e5a4825c5dd297cd
Mark Glenn <markglenn@gmail.com>
2020-08-08 18:29:43 -0500

    Fix linting error

--- lib/liquex/render/text.ex
+++ lib/liquex/render/text.ex
@@ -1,5 +1,10 @@
 defmodule Liquex.Render.Text do
+  @moduledoc """
+  Renderer for text blocks
+  """
   @behaviour Liquex.Render
+
+  @type text_t :: {:text, any}
 
   @impl Liquex.Render
   def render({:text, text}, context), do: {text, context}
