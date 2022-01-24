9359d55eb9df859fa475ad481c69834cda17bc70
cipater <cipater@users.noreply.github.com>
2022-01-24 15:22:00 -0800

    Implement and test support for Access.fetch/2
    Co-Authored-By: jdewar <1629311+jdewar@users.noreply.github.com>
    Co-Authored-By: Dan Porter <36777933+blackeyeddan@users.noreply.github.com>

--- test/liquex/indifferent_test.exs
+++ test/liquex/indifferent_test.exs
@@ -3,5 +3,22 @@ defmodule Liquex.IndifferentTest do
 
   use ExUnit.Case, async: true
 
+  defmodule StructWithAccess do
+    @behaviour Access
+
+    defstruct [:key]
+
+    def fetch(container, :key) do
+      {:ok, container.key <> " World"}
+    end
+
+    def fetch(_container, _), do: :error
+
+    def pop(container, _key), do: {container.key, container}
+
+    def get_and_update(container, _key, func),
+      do: {container.key, %{container | key: func.(container.key)}}
+  end
+
   doctest Liquex.Indifferent
 end
