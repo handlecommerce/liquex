5df3d1748ada806f16e497c326654d02acf43038
Mark Glenn <markglenn@gmail.com>
2022-09-28 13:27:11 -0500

    Add liquid tag parsing for for tag and render

--- test/liquex/tag/for_tag_test.exs
+++ test/liquex/tag/for_tag_test.exs
@@ -124,21 +124,25 @@ defmodule Liquex.Tag.ForTagTest do
           }
         })
 
-      {:ok, template} =
-        """
-        {% for product in collection.products %}
-          {{ product.title }}
-        {% endfor %}
-        """
-        |> String.trim()
-        |> Liquex.parse()
-
-      assert Liquex.render(template, context)
-             |> elem(0)
-             |> to_string()
-             |> String.trim()
+      assert render(
+               """
+               {% for product in collection.products %}
+                 {{ product.title }}
+               {% endfor %}
+               """,
+               context
+             )
              |> String.split("\n")
              |> trim_list() == ["hat", "shirt", "pants"]
+
+      assert render(
+               """
+               {% liquid for product in collection.products
+                 echo product.title
+               endfor %}
+               """,
+               context
+             ) == "hatshirtpants"
     end
 
     test "render loop with limit" do
@@ -159,6 +163,15 @@ defmodule Liquex.Tag.ForTagTest do
              |> String.trim()
              |> String.split("\n")
              |> trim_list() == ~w(1 2)
+
+      assert render(
+               """
+               {% liquid for item in array limit:2
+                 echo item
+               endfor %}
+               """,
+               context
+             ) == "12"
     end
 
     test "render loop with offset" do
@@ -179,6 +192,15 @@ defmodule Liquex.Tag.ForTagTest do
              |> String.trim()
              |> String.split("\n")
              |> trim_list() == ~w(3 4 5 6)
+
+      assert render(
+               """
+               {% liquid for item in array offset:2
+                 echo item
+               endfor %}
+               """,
+               context
+             ) == "3456"
     end
 
     test "render loop with reverse" do
@@ -199,6 +221,15 @@ defmodule Liquex.Tag.ForTagTest do
              |> String.trim()
              |> String.split("\n")
              |> trim_list() == ~w(6 5 4 3 2 1)
+
+      assert render(
+               """
+               {% liquid for item in array reversed
+                 echo item
+               endfor %}
+               """,
+               context
+             ) == "654321"
     end
 
     test "render loop with all the things" do
@@ -219,6 +250,15 @@ defmodule Liquex.Tag.ForTagTest do
              |> String.trim()
              |> String.split("\n")
              |> trim_list() == ~w(5)
+
+      assert render(
+               """
+               {% liquid for item in array reversed limit:2 offset:1
+                 echo item
+               endfor %}
+               """,
+               context
+             ) == "5"
     end
 
     test "render loop with range" do
@@ -237,6 +277,15 @@ defmodule Liquex.Tag.ForTagTest do
              |> String.trim()
              |> String.split("\n")
              |> trim_list() == ~w(3 4 5)
+
+      assert render(
+               """
+               {% liquid for i in (3..5)
+                 echo i
+               endfor %}
+               """,
+               Context.new(%{})
+             ) == "345"
     end
 
     test "render loop with range w/ field" do
@@ -249,12 +298,23 @@ defmodule Liquex.Tag.ForTagTest do
         |> String.trim()
         |> Liquex.parse()
 
-      assert Liquex.render(template, Context.new(%{"num" => 4}))
+      context = Context.new(%{"num" => 4})
+
+      assert Liquex.render(template, context)
              |> elem(0)
              |> to_string()
              |> String.trim()
              |> String.split("\n")
              |> trim_list() == ~w(1 2 3 4)
+
+      assert render(
+               """
+               {% liquid for i in (1..num)
+                echo i
+               endfor %}
+               """,
+               context
+             ) == "1234"
     end
 
     test "render for loop with forloop variable" do
@@ -279,6 +339,21 @@ defmodule Liquex.Tag.ForTagTest do
              |> String.trim()
              |> String.split("\n")
              |> trim_list() == ~w(true 1 0 false 3 3 2 false 2 1 false 3 2 1 false 3 2 true 3 1 0)
+
+      assert render(
+               """
+               {% liquid for i in (1..3)
+                 echo forloop.first
+                 echo forloop.index
+                 echo forloop.index0
+                 echo forloop.last
+                 echo forloop.length
+                 echo forloop.rindex
+                 echo forloop.rindex0
+               endfor %}
+               """,
+               Context.new(%{})
+             ) == "true10false332false21false321false32true310"
     end
 
     test "follows scoping rules" do
@@ -298,6 +373,16 @@ defmodule Liquex.Tag.ForTagTest do
              |> elem(0)
              |> to_string()
              |> String.trim() == "inner"
+
+      assert render("""
+             {% liquid
+              assign x = "outer"
+              for i in (1..1)
+                assign x = "inner"
+              endfor
+
+              echo x %}
+             """) == "inner"
     end
   end
 
